ea1058d4b00643e8aa6fc2a4935a91f25d344fed
Perry Brandiezs <pbrandiezs@gmail.com>
2018-07-03 13:46:58 -0700

    Initial commit

---
+++ Ex6.py
@@ -0,0 +1,4 @@
+the_string = input("Enter a string? ")
+backwards_string = the_string[::-1]
+if the_string == backwards_string:
+    print(the_string + " is a palindrome!")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-ex6
+https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
